d85bfcd3b0ed1abf2c708c67a708173f45ea71a9
Luke Ylias <luke.ylias@nib.com.au>
2024-03-07 15:26:20 +1100

    UI modifications

--- scripts.js
+++ scripts.js
@@ -41,3 +41,28 @@ document.addEventListener("DOMContentLoaded", () => {
     observer.observe(section);
   });
 });
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// Immediately hide the button on page load
+mybutton.style.display = "none"; // Ensure the button is hidden when the page loads
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
